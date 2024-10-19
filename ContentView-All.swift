27d00c1d323e91e3684a95e23dbc900b968ccc6e
cierrajenn <105517327+cierrajenn@users.noreply.github.com>
2024-10-19 17:55:10 -0400

    Update ContentView-All.swift to include Recipe Home and Create a Recipe pages

--- ContentView-All.swift
+++ ContentView-All.swift
@@ -233,7 +233,7 @@ struct BottomNavigationBar: View {
             }
             Spacer()
             // Recipe
-            NavigationLink(destination: RecipeView().navigationBarBackButtonHidden(true)) {
+            NavigationLink(destination: RecipeHomeView().navigationBarBackButtonHidden(true)) {
                 Image(systemName: "book.fill")
                     .foregroundColor(.black)
             }
@@ -363,6 +363,138 @@ struct CheckboxToggleStyle: ToggleStyle {
     }
 }
 
+// Cierra: Recipe Home page
+struct RecipeHomeView: View {
+    @State private var showRecipeCreateView = false
+    @State private var showRecipeView = false
+    
+    var body: some View {
+        NavigationStack() {
+            VStack() {
+                
+                // Button to Create Recipe page
+                NavigationLink(destination: RecipeCreateView().navigationBarBackButtonHidden(true)) {
+                    Text("Create a Recipe")
+                        .font(.headline)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue.opacity(0.2))
+                        .foregroundColor(.black)
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal, 40)
+                .padding(.bottom, 20)
+                
+                // Button to View a Recipe
+                NavigationLink(destination: RecipeView().navigationBarBackButtonHidden(true)) {
+                    Text("View Recipes")
+                        .font(.headline)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue.opacity(0.2))
+                        .foregroundColor(.black)
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal, 40)
+                .padding(.bottom, 20)
+            }
+        }
+        BottomNavigationBar()
+    }
+}
+
+// Cierra: Recipe Create page
+struct RecipeCreateView: View {
+    // TODO: implement logic to add buttons that allows them to be properly parsed and stored
+    // TODO: implement edit recipe functionality
+    @State private var recipeName = ""
+    @State private var newIngredient = ""
+    @State private var newInstruction = ""
+    @State private var ingredients : [String] =  []
+    @State private var instructions : [String] = []
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading, spacing: 20) {
+                //enter name for recipe
+                TextField("Enter Recipe Name", text: $recipeName)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                // add ingredients
+                VStack(alignment: .leading) {
+                    Text("Ingredients")
+                        .font(.headline)
+                    
+                    ForEach(ingredients, id: \.self) { ingredient in
+                        TextField("", text: .constant(ingredient))
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .disabled(true)
+                    }
+                    
+                    HStack {
+                        TextField("Add an Ingredient", text: $newIngredient)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        
+                        Button(action: {
+                            if !newIngredient.isEmpty {
+                                ingredients.append(newIngredient)
+                                newIngredient = ""
+                            }
+                        }) {
+                            Image(systemName: "plus.circle")
+                        }
+                    }
+                }.padding()
+                
+                // add instructions
+                VStack(alignment: .leading) {
+                    Text("Instructions")
+                        .font(.headline)
+                    
+                    ForEach(0 ..< instructions.count, id: \.self) { index in
+                        HStack(alignment: .top) {
+                            Text("\(index + 1).")
+                                .bold()
+                            TextField("", text: .constant(instructions[index]))
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .disabled(true)
+                        }
+                    }
+                    
+                    HStack {
+                        TextField("Add an Instruction", text: $newInstruction)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        
+                        Button(action: {
+                            if !newInstruction.isEmpty {
+                                instructions.append(newInstruction)
+                                newInstruction = ""
+                            }
+                        }) {
+                            Image(systemName: "plus.circle")
+                        }
+                    }
+                }.padding()
+                
+                Spacer()
+            }
+        }
+        // when Done is clicked, this will redirect to recipe home and eventually save recipe info
+        NavigationLink(destination: RecipeHomeView().navigationBarBackButtonHidden(true)) {
+            Text("Done")
+                .font(.headline)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.blue.opacity(0.2))
+                .foregroundColor(.black)
+                .cornerRadius(10)
+        }
+        .padding(.horizontal, 40)
+        .padding(.bottom, 20)
+    }
+}
+
 // Abi: Appliances page
 struct ApplianceView: View {
     // TODO: This will need to be figured out: type of appliances, only allow one button
@@ -482,7 +614,7 @@ struct RecipeView: View {
                     
                 }
             }
-            BottomNavigationBar()
+            //BottomNavigationBar()
             }
             
         }
