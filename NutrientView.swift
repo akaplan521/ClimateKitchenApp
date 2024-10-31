66a53c3ca57e50d2c0029c8740ff8dea8bb7dc59
cierrajenn <105517327+cierrajenn@users.noreply.github.com>
2024-10-31 10:20:12 -0400

    Create NutrientView.swift

---
+++ NutrientView.swift
@@ -0,0 +1,9 @@
+//Abi:
+// Nutrition facts page
+struct NutrientView: View {
+    var home : ContentView!
+    // TODO: Nutrition for this Ingredient
+    var body: some View {
+        Text("FACTS").lineSpacing(10)
+    }
+}
