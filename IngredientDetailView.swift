7dde8c00ca0a6f851062054aa6bf4beefe835893
cierrajenn <105517327+cierrajenn@users.noreply.github.com>
2024-10-31 10:15:57 -0400

    Create IngredientDetailView.swift

---
+++ IngredientDetailView.swift
@@ -0,0 +1,18 @@
+struct IngredientDetailView: View {
+    let ingredient: Ingredient
+
+    var body: some View {
+        VStack {
+            Text(ingredient.name)
+                .font(.largeTitle)
+                .padding()
+
+            Text(ingredient.info)
+                .font(.body)
+                .padding()
+
+            Spacer()
+        }
+        .navigationTitle("Ingredient Info")
+    }
+}
