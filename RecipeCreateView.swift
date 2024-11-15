0ad047be24b6bee7e083a56f2bb71a3cb8676e6f
Alexa Kaplan <113000702+akaplan521@users.noreply.github.com>
2024-11-15 15:57:06 -0500

    Added notes section and scroll view

--- RecipeCreateView.swift
+++ RecipeCreateView.swift
@@ -3,85 +3,127 @@ import UIKit
 
 // Cierra: Recipe Create page
 struct RecipeCreateView: View {
+    //---------------------------------------------------------------------------------------
+    
+    
+    //need to add prep and quantity for ingredients.
+    
+    //-----------------------------------------------------------------------------------------
+    
+    
     // TODO: implement logic to add buttons that allows them to be properly parsed and stored
     // TODO: implement edit recipe functionality
     @State private var recipeName = ""
     @State private var newIngredient = ""
     @State private var newInstruction = ""
+    @State private var newNotes = ""
     @State private var ingredients : [String] =  []
     @State private var instructions : [String] = []
+    @State private var notes : [String] = []
 
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading, spacing: 20) {
-                //enter name for recipe
-                TextField("Enter Recipe Name", text: $recipeName)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                
-                // add ingredients
-                VStack(alignment: .leading) {
-                    Text("Ingredients")
-                        .font(.headline)
+            ScrollView{
+                VStack(alignment: .leading, spacing: 20) {
+                    //enter name for recipe
+                    TextField("Enter Recipe Name", text: $recipeName)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
                     
-                    ForEach(ingredients, id: \.self) { ingredient in
-                        TextField("", text: .constant(ingredient))
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                            .disabled(true)
-                    }
-                    
-                    HStack {
-                        TextField("Add an Ingredient", text: $newIngredient)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    // add ingredients
+                    VStack(alignment: .leading) {
+                        Text("Ingredients")
+                            .font(.headline)
                         
-                        Button(action: {
-                            if !newIngredient.isEmpty {
-                                ingredients.append(newIngredient)
-                                newIngredient = ""
-                            }
-                        }) {
-                            Image(systemName: "plus.circle")
-                        }
-                    }
-                }.padding()
-                
-                // add instructions
-                VStack(alignment: .leading) {
-                    Text("Instructions")
-                        .font(.headline)
-                    
-                    ForEach(0 ..< instructions.count, id: \.self) { index in
-                        HStack(alignment: .top) {
-                            Text("\(index + 1).")
-                                .bold()
-                            TextField("", text: .constant(instructions[index]))
+                        ForEach(ingredients, id: \.self) { ingredient in
+                            TextField("", text: .constant(ingredient))
                                 .textFieldStyle(RoundedBorderTextFieldStyle())
                                 .disabled(true)
                         }
-                    }
-                    
-                    HStack {
-                        TextField("Add an Instruction", text: $newInstruction)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
                         
-                        Button(action: {
-                            if !newInstruction.isEmpty {
-                                instructions.append(newInstruction)
-                                newInstruction = ""
+                        HStack {
+                            TextField("Add an Ingredient", text: $newIngredient)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                            
+                            Button(action: {
+                                if !newIngredient.isEmpty {
+                                    ingredients.append(newIngredient)
+                                    newIngredient = ""
+                                }
+                            }) {
+                                Image(systemName: "plus.circle")
                             }
-                        }) {
-                            Image(systemName: "plus.circle")
                         }
-                    }
-                }.padding()
-                
-                Spacer()
-            }
+                    }.padding()
+                    
+                    // add instructions
+                    VStack(alignment: .leading) {
+                        Text("Instructions")
+                            .font(.headline)
+                        
+                        ForEach(0 ..< instructions.count, id: \.self) { index in
+                            HStack(alignment: .top) {
+                                Text("\(index + 1).")
+                                    .bold()
+                                TextField("", text: .constant(instructions[index]))
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                    .disabled(true)
+                            }
+                        }
+                        
+                        HStack {
+                            TextField("Add an Instruction", text: $newInstruction)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                            
+                            Button(action: {
+                                if !newInstruction.isEmpty {
+                                    instructions.append(newInstruction)
+                                    newInstruction = ""
+                                }
+                            }) {
+                                Image(systemName: "plus.circle")
+                            }
+                        }
+                    }.padding()
+                    // add notes
+                    VStack(alignment: .leading) {
+                        Text("Notes")
+                            .font(.headline)
+                        
+                        ForEach(0 ..< notes.count, id: \.self) { index in
+                            HStack(alignment: .top) {
+                                Text("\(index + 1).")
+                                    .bold()
+                                TextField("", text: .constant(notes[index]))
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                    .disabled(true)
+                            }
+                        }
+                        
+                        HStack {
+                            TextField("Add an Instruction", text: $newNotes)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                            
+                            Button(action: {
+                                if !newNotes.isEmpty {
+                                    notes.append(newNotes)
+                                    newNotes = ""
+                                }
+                            }) {
+                                Image(systemName: "plus.circle")
+                            }
+                        }
+                    }.padding()
+                    
+                    Spacer()
+                    Spacer()
+                }}
         }
         // when Done is clicked, this will redirect to recipe home and eventually save recipe info
         Button("Done") {
+            //Call function for adding recipe to sql
             dismiss()
         }
         .font(.headline)
