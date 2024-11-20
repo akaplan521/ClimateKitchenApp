e553b95d0b6d8b326719dae8c7b4392356c788bc
cierrajenn <105517327+cierrajenn@users.noreply.github.com>
2024-11-19 20:28:51 -0500

    removed rating features from RecipeView, moved to ReviewView

--- RecipeView.swift
+++ RecipeView.swift
@@ -12,6 +12,7 @@ import UIKit
 struct CheckboxToggleStyle: ToggleStyle {
     func makeBody(configuration: Configuration) -> some View {
         HStack {
+            
             RoundedRectangle(cornerRadius: 5.0)
                 .stroke(lineWidth: 2)
                 .frame(width: 25, height: 25)
@@ -32,78 +33,57 @@ struct CheckboxToggleStyle: ToggleStyle {
 
 struct RecipeView: View {
     @EnvironmentObject var settings: Settings
+    @State private var showReviewView = false
+    @State private var seasonal = [true, true,true, false, false]
     @State private var fillColor = [Color.black,Color.black,Color.black,Color.black,Color.black]
     @State private var isEditing = false
     @State private var isTapped = false
     @State private var preheatTime: Int?
-    //TODO: These will get read in from database, is checked will be all set to false with length of ingredients
     @State private var isChecked = [false,false,false,false, false]
+    
     let ingredients = [("1","Carrot"), ("2-3","Beets"),("1", "Parsnip"),("3-4 T","Olive Oil" ), ("Dash", "Salt")]
     let recipeTitle = "Roasted Root Vegetables"
     let prepTime = 20
     let cookTime = 30
-    @State private var sesonal = [true, true,true, false, false]
     let instructions = [("1.", "Preheat the oven to 425°F."),
                         ("2.","Wash, peel and cut veggies."),
                         ("3.", "On a low-sided baking sheet, toss veggies together with salt and olive oil. Spread them out and roast until browned and tender, 25-30 minutes.")]
     // This calculates the BTU used from appliance type, preheat time, and cook time
     func calculateBTU (){
-        // default
+        // default BTUs
         if preheatTime == nil {
             preheatTime = 0
         }
-        if settings.applianceType == "Gas"{
-            
-            settings.btuUsed = 300.0*Float(preheatTime!+cookTime)
+        
+        else if settings.applianceType == "Gas"{
+            settings.btuUsed = 300.0 * Float(preheatTime! + cookTime)
         }
-        if settings.applianceType == "Electric"{
-            settings.btuUsed = 130.883925*Float(preheatTime!+cookTime)
+        
+        else if settings.applianceType == "Electric"{
+            settings.btuUsed = 130.883925 * Float(preheatTime! + cookTime)
         }
-        if settings.applianceType == "Induction"{
-            settings.btuUsed = 283.33333*Float(preheatTime!+cookTime)
+        
+        else if settings.applianceType == "Induction"{
+            settings.btuUsed = 283.33333 * Float(preheatTime! + cookTime)
             
         }
         print(settings.btuUsed)
     }
     
-    // this function changes the star colors
-    func changeStars(num : Int){
-       
-        if fillColor[num] == Color.black{
-            for number in 0...num{
-                if fillColor[number] == Color.black{
-                    fillColor[number] = Color.yellow
-                }
-            }
-        }
-        else{
-            for number in num...4{
-                if fillColor[number] == Color.yellow{
-                    fillColor[number] = Color.black
-                }
-            }
-        }
-        var ratingCount = 0
-        for i in 0...4{
-            if fillColor[i] == Color.yellow{
-                ratingCount+=1
-            }
-        }
-        settings.rating = ratingCount
-            }
-   // calculate local percentage
+    // calculate local percentage
     func calculateLocal() {
-        var totalLocal = 0
+        var localCount = 0
         let totalIngredients = isChecked.count
         for ingredient in isChecked{
             if ingredient == true{
-                totalLocal += 1
+                localCount += 1
             }
         }
-        var tot = (Float(totalLocal) / Float(totalIngredients))
-        settings.localPercent = tot*100
+        var localPercent = (Float(localCount) / Float(totalIngredients))
+        settings.localPercent = localPercent * 100
         print(settings.localPercent)
     }
+   
     
     var body: some View {
         
@@ -119,24 +99,23 @@ struct RecipeView: View {
                                                     .resizable()
                                                     .aspectRatio(contentMode: .fit)
                                                     .padding()
-                            // TODO: ingredients, centered it but can change
+                            // ingredients, centered it but can change
                             Text("Ingredients").bold().frame(maxWidth: .infinity)
-                            Text("Check If Local")
                             ForEach(ingredients.indices, id: \.self) { index in
                                 HStack {
                                     // Toggle check box and if ingredient is seasonal or not
                                     // TODO: change color to display image
                                     Toggle(isOn: $isChecked[index]) {
-                                        if sesonal[index] == true{
+                                        if seasonal[index] == true{
                                             Text(self.ingredients[index].0)
                                             NavigationLink(destination: NutrientView()) {
-                                                    Text(ingredients[index].1).foregroundColor(.green)
+                                                    Text(ingredients[index].1)
                                             }
                                                 }
                                             else{
                                                 Text(self.ingredients[index].0)
                                                 NavigationLink(destination: NutrientView()) {
-                                                    Text(ingredients[index].1).foregroundColor(.red)
+                                                    Text(ingredients[index].1)
                                                 }
                                         }
                                         
@@ -144,7 +123,7 @@ struct RecipeView: View {
                                 }
                             }
                             
-                            // TODO: Instructions, centered it but can change
+                            // instructions, centered it but can change
                             Text("Instructions").bold().frame(maxWidth: .infinity)
                             ForEach(instructions.indices, id: \.self) { index in
                                 // if instrucstions contain oven or stove, but can be other appliances
@@ -167,112 +146,15 @@ struct RecipeView: View {
                                 }
                             }
                             TextField("Oven Preheat Time", value: $preheatTime, formatter: NumberFormatter())
-                            // sliders for users to pick
-                            // TODO: difficulty, centered it but can change
-                            Text("Difficulty").bold().frame(maxWidth: .infinity)
-                            HStack(){
-                                Text("Easy")
-                                Spacer()
-                                Text ("Hard")
-                            }.scenePadding(.top)
-                            Slider(
-                                value: $settings.difficulty,
-                                in: 0...5,
-                                onEditingChanged: { editing in
-                                    isEditing = editing
-                                    
-                                }
-                            )
-                            // TODO: temperature, centered it but can change
-                            Text("Temperature").bold().frame(maxWidth: .infinity)
-                            HStack(){
-                                Text("Hot")
-                                Spacer()
-                                Text ("Cold")
-                            }.scenePadding(.top)
+
                             
-                            Slider(
-                                value: $settings.foodTemp,
-                                in: 0...5,
-                                onEditingChanged: { editing in
-                                    isEditing = editing
-                                    
-                                }
-                            )
-                            // TODO: Spice, centered it but can change
-                            Text("Spice Level").bold().frame(maxWidth: .infinity)
-                            HStack(){
-                                Text("Not Spicy")
-                                Spacer()
-                                Text ("Spicy")
-                            }.scenePadding(.top)
-                            
-                            Slider(
-                                value: $settings.spiceLevel,
-                                in: 0...5,
-                                onEditingChanged: { editing in
-                                    isEditing = editing
-                                    
-                                }
-                            )
-                            Text("Click On The Food Descriptors Below and Choose the Relevant Boxes").bold().frame(maxWidth: .infinity)
-                            
-                            
-                            NavigationLink(destination: Appearance().environmentObject(Settings())) {
-                                Text("Appearance").bold().frame(maxWidth: .infinity)
-                            }
-                            
-                            
-                            NavigationLink(destination: Armoa().environmentObject(Settings())) {
-                                Text("Aroma").bold().frame(maxWidth: .infinity)
-                            }
-                            
-                            
-                            NavigationLink(destination: Flavor().environmentObject(Settings())) {
-                                Text("Flavor").bold().frame(maxWidth: .infinity)
-                            }
-                            
-                            
-                            NavigationLink(destination: Texture().environmentObject(Settings())) {
-                                Text("Texture").bold().frame(maxWidth: .infinity)
-                            }
-                            HStack{
-                                Text("Tap For Rating").bold()
-                                // 1 star ranking
-                                Image(systemName: "star.fill").foregroundColor(fillColor[0]).onTapGesture(count: 2) {
-                                    changeStars(num:0)
-                                }
-                                // 2 star ranking
-                                Image(systemName: "star.fill").foregroundColor(fillColor[1]).onTapGesture(count: 2) {
-                                    changeStars(num:1)
-                                }
-                                // 3 star ranking
-                                Image(systemName: "star.fill").foregroundColor(fillColor[2]).onTapGesture(count: 2) {
-                                    changeStars(num:2)
-                                }
-                                // 4 star ranking
-                                Image(systemName: "star.fill").foregroundColor(fillColor[3]).onTapGesture(count: 2) {
-                                    changeStars(num:3)
-                                }
-                                // 5 star ranking
-                                Image(systemName: "star.fill").foregroundColor(fillColor[4]).onTapGesture(count: 2) {
-                                    changeStars(num : 4)
-                                }
-                                
-                            }.frame(maxWidth:.infinity)
-                            
-                                                              
-                            // TODO: add struct to do the math, then direct to home page
-                            HStack(spacing:100){
-                                // Add all the stats to profile
-                                Button("I MADE THIS"){calculateBTU ()
-                                    calculateLocal()
-                                    
-                                    
-                                }
-                                NavigationLink(destination: HomeView().environmentObject(Settings()).navigationBarBackButtonHidden(true)){
-                                    
-                                }
+                            Button(action: {
+                                calculateBTU()
+                                calculateLocal()
+                                showReviewView = true}) {
+                                Text("I Made This!")
+                            }.navigationDestination(isPresented: $showReviewView) {
+                                ReviewView().environmentObject(Settings()).navigationBarBackButtonHidden(true)
                             }
                             
                         }
