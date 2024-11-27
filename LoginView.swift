d06a36ef647223489d0945523f1ee55030705a4e
Catie Crowell <112882114+cwcrowel@users.noreply.github.com>
2024-11-26 22:52:40 -0500

    two different screens for login and create account

--- LoginView.swift
+++ LoginView.swift
@@ -2,51 +2,50 @@
 import SwiftUI
 import UIKit
 
-// formatting
-struct LoginCards: View {
-    let iconName: String
-    let description: String
-    let hunterGreen = Color(red: 20/255, green: 71/255, blue: 52/255)
-    
-    var body: some View {
-        HStack {
-            Image(systemName: iconName)
-                .font(.largeTitle)
-                .frame(width: 50)
-                .padding(.trailing, 10)
-            
-            // TODO: change navLink to two separate sign in pages?
-            NavigationLink(destination: SignInEmailView(showSignInView: .constant(false)).navigationBarBackButtonHidden(true)) {
-                Text(description)
-            }
-            
-            Spacer()
-        }
-        .padding()
-        .background(Color(hunterGreen), in: RoundedRectangle(cornerRadius: 12))
-        .foregroundStyle(.white)
-    }
-}
-
 // login page
 struct LoginView: View {
+    let hunterGreen = Color(red: 20/255, green: 71/255, blue: 52/255)
+    
     var body: some View {
         VStack {
             Text("Sign in")
                 .font(.title)
                 .fontWeight(.semibold)
-            
-            LoginCards(iconName: "person.2.circle.fill",
-                        description: "Login")
-            
-            LoginCards(iconName: "person.crop.rectangle.badge.plus.fill",
-                        description: "Create Account")
+
+            // sign into account
+            HStack {
+                Image(systemName: "person.2.circle.fill")
+                    .font(.largeTitle)
+                    .frame(width: 50)
+                    .padding(.trailing, 10)
+                
+                NavigationLink(destination: SignInEmailView(showSignInView: .constant(true)).navigationBarBackButtonHidden(true)) {
+                    Text("Login")
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .background(Color(hunterGreen), in: RoundedRectangle(cornerRadius: 12))
+            .foregroundStyle(.white)
+
+            // create account
+            HStack {
+                Image(systemName: "person.crop.rectangle.badge.plus.fill")
+                    .font(.largeTitle)
+                    .frame(width: 50)
+                    .padding(.trailing, 10)
+                
+                NavigationLink(destination: SignUpEmailView(showSignInView: .constant(true)).navigationBarBackButtonHidden(true)) {
+                    Text("Create Account")
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .background(Color(hunterGreen), in: RoundedRectangle(cornerRadius: 12))
+            .foregroundStyle(.white)
         }
         .padding()
     }
-}
-
-
-#Preview {
-    LoginView()
 }
