ba6e97d5dbf196151feb451fc912150d5fc87d10
Catie Crowell <112882114+cwcrowel@users.noreply.github.com>
2024-11-26 22:50:17 -0500

    Added transitions, changed font, deleted preview

--- WelcomeView.swift
+++ WelcomeView.swift
@@ -5,22 +5,33 @@ import UIKit
 
 // Logo page
 struct WelcomeView: View {
+    @State private var showImage = false
+    @State private var showText = false
+    
     var body: some View {
         VStack {
             Image(uiImage: UIImage(named: "uvmLogo")!)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .padding()
+                .offset(y: showImage ? 0 : -400)
+                .animation(.easeOut(duration: 0.8), value: showImage)
             
             Text("Climate Kitchen App")
                 .font(.title)
                 .fontWeight(.semibold)
                 .padding(.top)
+                .offset(y: showText ? 0 : 400)
+                .animation(.easeOut(duration: 0.8).delay(0.2), value: showText)
             
         }
         
         .padding()
-        
+        .onAppear {
+            // trigger animations
+            showImage = true
+            showText = true
+        }
     }
 }
 
@@ -38,8 +49,4 @@ struct WelcomeToOurAppView: View {
         .background(Gradient(colors: gradientColors))
                 .tabViewStyle(.page)
     }
-}
-
-#Preview {
-    WelcomeToOurAppView()
 }
